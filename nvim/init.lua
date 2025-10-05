588750daf1f5c4cc3efdc7c2f7a8ea7379c70123
ronit gandhi <ronitgandhi96@gmail.com>
2025-10-05 17:18:30 +0530

    version bump up

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- Disable backup files
-vim.opt.backup = false
-vim.opt.writebackup = false
-
--- Disable undo files
-vim.opt.undofile = false
